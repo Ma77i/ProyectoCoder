db69a0c679d641acf003748c27e9ace65be2a0c5
Ma77i <mattiasuru@gmail.com>
2021-04-15 14:20:13 -0300

    agregado scroll reveal

--- js/Javascript.js
+++ js/Javascript.js
@@ -1,6 +1,11 @@
+ScrollReveal().reveal('.headline',{
+  duration: 3000
+});
+
 var myModal = document.getElementById('myModal')
 var myInput = document.getElementById('myInput')
 
 myModal.addEventListener('shown.bs.modal', function () {
   myInput.focus()
 })
+

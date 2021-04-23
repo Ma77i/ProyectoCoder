1b889052d402d30fb0dce3ec148e764ea614832f
Ma77i <mattiasuru@gmail.com>
2021-04-23 15:48:09 -0300

    final

--- js/Javascript.js
+++ js/Javascript.js
@@ -5,7 +5,3 @@ ScrollReveal().reveal('.headline',{
 var myModal = document.getElementById('myModal')
 var myInput = document.getElementById('myInput')
 
-myModal.addEventListener('shown.bs.modal', function () {
-  myInput.focus()
-})
-
